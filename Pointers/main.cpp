53f5bc0f1c62ace7514e09b63f78449683dca092
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-05-11 19:28:18 +0300

    TransferParameters by value

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -1,8 +1,35 @@
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define POINTERS_BASICS
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
+#ifdef POINTERS_BASICS
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;  //ѕолучаем адрес переменной 'a' пр€мо при выводе
+	cout << pa << endl;  //¬ывод адреса, хран€щегос€ в указателе
+	cout << *pa << endl; //–азыменовываем уазатель 'pa' и получаем значение хран€щеес€ по адресу
+
+	int* pb;
+	int b = 3;
+	pb = &b;
+#endif // POINTERS_BASICS
+
+	const int n = 5;
+	int arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
 }
