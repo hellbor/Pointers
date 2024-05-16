a53e172e666d87f6f851e1013d38aedfc8f8a8f5
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-05-16 22:05:53 +0300

    push_back DONE

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
+int* push_back(int arr[], int& n, const int value);
 
 void main()
 {
@@ -19,19 +20,7 @@ void main()
 
 	int value;
 	cout << "Введите добавляемое значение:"; cin >> value;
-	int* buffer = new int[n + 1];
-	for (int i = 0; i < n; i++)
-	{
-		buffer[i] = arr[i];
-	}
-	delete[] arr;
-	int a = 2;
-	int b = 3;
-	a = b;
-	arr = buffer;
-	arr[n] = value;
-	n++;
-
+	arr = push_back(arr, n, value);
 	Print(arr, n);
 
 	delete[] arr;
@@ -54,4 +43,20 @@ void Print(int arr[], const int n)
 		//Обращение к элементам массива через оператор индексирования []
 	}
 	cout << endl;
+}
+int* push_back(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	int a = 2;
+	int b = 3;
+	a = b;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	return arr;
 }
