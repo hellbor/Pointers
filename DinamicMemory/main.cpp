c78a99673febac092a14736492be1bbc3d27c8f8
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-05-18 18:55:35 +0300

    Push DONE

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include<stack>
 
 using namespace std;
 
@@ -6,7 +7,12 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
+
 int* push_back(int arr[], int& n, const int value);
+int* push_front(int arr[], int& n, const int value);
+
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
 
 void main()
 {
@@ -21,6 +27,14 @@ void main()
 	int value;
 	cout << "Введите добавляемое значение:"; cin >> value;
 	arr = push_back(arr, n, value);
+	Print(arr, n);
+
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+	arr = pop_front(arr, n);
 	Print(arr, n);
 
 	delete[] arr;
@@ -59,4 +73,36 @@ int* push_back(int arr[], int& n, const int value)
 	arr[n] = value;
 	n++;
 	return arr;
+}
+int* push_front(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i + 1];
+	}
+	delete[] arr;
+	return buffer;
 }
